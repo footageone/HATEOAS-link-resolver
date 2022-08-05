8cb42bf31b54a1d1355d36fdcb2e53c863bb9ee4
Daniel Schuba <daniel.schuba@footage.one>
2022-08-05 09:08:26 +0200

    Fix testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['<rootDir>/test/**/*.test.ts'],
-    testPathIgnorePatterns: ['/node_modules/'],
-    coverageDirectory: './coverage',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage',
 };
